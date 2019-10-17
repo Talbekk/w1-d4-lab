85071b918578a2b0607fd14508410621168564ac
Talbekk <mt.campbell@outlook.com>
2019-10-17 14:09:10 +0100

    Q1-Q5

--- starting_point/friends.rb
+++ starting_point/friends.rb
@@ -14,3 +14,17 @@ def likes_to_eat(person, fave_food)
   end
     return false
 end
+
+def add_friend(person, friend_name)
+  person[:friends].push(friend_name)
+  return person[:friends].length()
+end
+
+def remove_friend(person, friend_name)
+  for friend in person[:friends]
+    if ( friend == friend_name )
+      person[:friends].delete(friend)
+    end
+  end
+  return person[:friends].length() 
+end
